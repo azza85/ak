0884dd9afa0425261fea8787469a7386aa84426d
aaronk85 <aaronkenez@gmail.com>
2020-02-12 14:22:59 +1100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aaron Kenez`,
+    description: `Aaron Kenez Portfolio Site.`,
+    author: `@azza85`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
